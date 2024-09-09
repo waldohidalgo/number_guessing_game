ee68b82b7864315ddfda7be75fff8ca49bc9f959
Waldo Hidalgo <whidalgohp@gmail.com>
2024-09-09 23:17:10 +0000

    feat: query and show game user data

--- number_guess.sh
+++ number_guess.sh
@@ -10,5 +10,13 @@ if [[ -z $USERNAME ]]; then
     echo "Error: Username cannot be empty"
 else
     SEARCH_USERNAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
-    
+    if [[ -z $SEARCH_USERNAME ]]; then
+        echo "Welcome, $USERNAME! It looks like this is your first time here."
+    else
+        QUERY_GAMES=$($PSQL "SELECT count(*), MIN(games_played) FROM games INNER JOIN users ON games.user_id=users.user_id WHERE username='$USERNAME'")
+        echo "$QUERY_GAMES" | while IFS="|" read GAMES_PLAYED BEST_SCORE; do
+            echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_SCORE guesses."
+        done
+        
+    fi
 fi
