506bdb7c38ea8a1fac52b761229654814b01f6b1
Waldo Hidalgo <whidalgohp@gmail.com>
2024-09-09 23:43:38 +0000

    fix: fix writing bugs

--- number_guess.sh
+++ number_guess.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
 GUESS_NUMBER() {
     USERNAME=$1
     echo "Guess the secret number between 1 and 1000:"
     NUMBER_OF_GUESSES=0
     read NUMBER_INPUT
+    NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES + 1))
     while [[ ! $NUMBER_INPUT =~ ^[0-9]+$ ]]; do
         echo "That is not an integer, guess again:"
         read NUMBER_INPUT
@@ -29,23 +32,24 @@ GUESS_NUMBER() {
     INSERT_GAME=$($PSQL "INSERT INTO games(user_id, games_played, number_guess) VALUES((SELECT user_id FROM users WHERE username='$USERNAME'), $NUMBER_OF_GUESSES, $SECRET_NUMBER)")
 }
 
-$SECRET_NUMBER=$(((RANDOM % 1000) + 1))
+SECRET_NUMBER=$((1 + $RANDOM % 1000))
 
 echo "Enter your username:"
 
-READ USERNAME
+read USERNAME
 
 if [[ -z $USERNAME ]]; then
     echo "Error: Username cannot be empty"
 else
-    SEARCH_USERNAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+    SEARCH_USERNAME=$($PSQL "SELECT user_id FROM users WHERE username='$USERNAME'")
     if [[ -z $SEARCH_USERNAME ]]; then
         echo "Welcome, $USERNAME! It looks like this is your first time here."
+        INSERT_USER=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
     else
         QUERY_GAMES=$($PSQL "SELECT count(*), MIN(games_played) FROM games INNER JOIN users ON games.user_id=users.user_id WHERE username='$USERNAME'")
         echo "$QUERY_GAMES" | while IFS="|" read GAMES_PLAYED BEST_SCORE; do
             echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_SCORE guesses."
         done
-        
     fi
+    GUESS_NUMBER $USERNAME
 fi
