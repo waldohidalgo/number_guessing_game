c7448d73f6c6d12771c13fdab6b693345bf30d97
Waldo Hidalgo <whidalgohp@gmail.com>
2024-09-09 23:16:04 +0000

    feat: handle username

--- number_guess.sh
+++ number_guess.sh
@@ -5,3 +5,10 @@ $SECRET_NUMBER=$(((RANDOM % 1000) + 1))
 echo "Enter your username:"
 
 READ USERNAME
+
+if [[ -z $USERNAME ]]; then
+    echo "Error: Username cannot be empty"
+else
+    SEARCH_USERNAME=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'")
+    
+fi
